bde3eb9b781895cf4245f8ea4b002953bad415bb
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-01-05 23:19:47 +0330

    create first views and models.

--- login/models.py
+++ login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class SessionModel(models.Model):
+    phone_number = models.CharField(max_length=20)
+    last_verification_code = models.CharField(max_length=5)
+    verification_time = models.DateTimeField(auto_now_add=True)
+    session_file = models.FileField(upload_to='session_files/')
--- login/views.py
+++ login/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse
+from .forms import PhoneNumberForm, VerificationCodeForm
 
 # Create your views here.
+class PhoneNumberView(CreateView):
+    template_name = 'login/phone_number.html'
+    form_class = PhoneNumberForm
+    success_url = reverse('login:verification_code')
+
+    def form_valid(self, form):
+        form.send_verification_code()
+        return super().form_valid(form)
+
+
+class VerificationCodeView(CreateView):
+    template_name = 'login/verification_code.html'
+    form_class = VerificationCodeForm
+    success_url = reverse('home')
+
+    def form_valid(self, form):
+        form.save_session_file()
+        return super().form_valid(form)
+
+    
